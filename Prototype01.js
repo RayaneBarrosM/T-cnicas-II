f803898270576fb88166f53ff7b2738faf66301a
Rayane <164011441+RayaneBarrosM@users.noreply.github.com>
2025-03-26 15:42:06 -0300

    Update Prototype01.js
    funcional

--- Prototype01.js
+++ Prototype01.js
@@ -1,61 +1,59 @@
 //Classe Pessoa -  referencia para ser clonada:
-class Pessoa{
-    constructor(id,nome,idade){
-        this.id = id;
-        this.nome = nome;
-        this.idade = idade;
-    }
+class Pessoa {
+  constructor(id, nome, idade) {
+    this.id = id;
+    this.nome = nome;
+    this.idade = idade;
+  }
 
-
-//metodo para copiar os Objetos:
-    Clone(){
-        return new Pessoa(this.id, this.nome, this.idade)
-    }
+  //Metodo para copiar os Objetos:
+  clone() {
+    return new Pessoa(this.id, this.nome, this.idade);
+  }
 }
 
 //Classe gerenciamentoPessa para erenciar instancia Pessoa:
-class gerenciamentoPessa{
-    constructor(){
-        this.pessoas = {}
-    }
+class gerenciamentoPessa {
+  constructor() {
+    this.pessoas = {};
+  }
 
-    //add uma nova pessoa ao dicionario Pessoas:
-    addPessoa(id, nome, idade){
-        const pessoa = new Pessoa(id,nome,idade);
-        this.pessoas[id] = pessoa;
-    } 
+  //Add uma nova pessoa ao dicionario Pessoas:
+  addPessoa(id, nome, idade) {
+    const pessoa = new Pessoa(id, nome, idade);
+    this.pessoas[id] = pessoa;
+  }
 
-    //solicitar uma pessoa pelo id e retorn uma copia dela
-    getPessoaById(id){
-        const pessoaOriginal = this.pessoas[id];
-        if(pessoaOriginal){
-            return pessoaOriginal.clone();
-        }else{
-            return null;
-        }
+  //Solicitar uma pessoa pelo id e retorn uma copia dela
+  getPessoaById(id) {
+    const pessoaOriginal = this.pessoas[id];
+    if (pessoaOriginal) {
+      return pessoaOriginal.clone(); // Now matches the method name
+    } else {
+      return null;
     }
+  }
 }
 
-//criando uma instancia de gerenciaPessoa:
+//Criando uma instancia de gerenciaPessoa:
 
-const manager = gerenciamentoPessa = new gerenciamentoPessa();
+const manager = new gerenciamentoPessa();
 
-//add pessoas:
+//Add pessoas:
 
-manager.addPessoa(1, "Ana", 19);
-manager.addPessoa(2, "guilherme", 20);
+manager.addPessoa(1, 'Ana', 19);
+manager.addPessoa(2, 'guilherme', 20);
 
-//clonado pessoas e modificando copias:
+//Clonado pessoas e modificando copias:
 const pessoaClone1 = manager.getPessoaById(1);
-if(pessoaClone1){
-    pessoaClone1.nome = "Natali"
+if (pessoaClone1) {
+  pessoaClone1.nome = 'Natali';
 }
 
+//Imprimindo as duas pessoas
 
-//imprimindo as duas pessoas
-
-console.log("Pessoa Original: ")
+console.log('Pessoa Original: ');
 console.log(manager.getPessoaById(1));
 
-console.log("Pessoa clonada: ")
+console.log('Pessoa clonada: ');
 console.log(pessoaClone1);
