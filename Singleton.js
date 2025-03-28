b3d8c2a27907d2bd475670da38d4fc80130cf48c
Rayane <164011441+RayaneBarrosM@users.noreply.github.com>
2025-03-28 11:57:13 -0300

    Create Singleton.js

---
+++ Singleton.js
@@ -0,0 +1,6 @@
+/* Atividade:
+- Factory Method / Abstract Factory - Escolher a seleção de qual tipo de serviço será fornecido (Pizzaria ou Hamburgueria por exemplo).
+
+- Prototype / Builder - Montagem da Pizza
+
+- Singleton - Processo de montagem do pedido (chamando apenas uma instancia para criar a pizza e inserir no pedido).*/
